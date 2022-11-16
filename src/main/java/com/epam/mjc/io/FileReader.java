577bfd5c08e978df15b9b6aabe68fe695f4b482a
alexdarkovich <alexdarkovich@mail.ru>
2022-11-16 14:29:42 +0300

    and once aggain

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,11 +2,9 @@ package com.epam.mjc.io;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 import java.util.Set;
 
 
@@ -15,13 +13,12 @@ public class FileReader {
 
     public static String returnValueFromMap(String str, Map<String, String> map) {
 
-        Set<Map.Entry<String, String>> entrySet=map.entrySet();
+        Set<Map.Entry<String, String>> entrySet = map.entrySet();
         String toReturn = null;
 
-        for (Map.Entry<String,String> pair : entrySet) {
+        for (Map.Entry<String, String> pair : entrySet) {
             if (str.equals(pair.getKey())) {
                 toReturn = pair.getValue();
-
 
             }
         }
@@ -29,22 +26,19 @@ public class FileReader {
     }
 
     public static Profile getDataFromFile(File file) throws IOException {
-//        file = new File("src\\main\\resources\\Profile.txt");
         Map<String, String> map = new HashMap<>();
 
-        try (java.io.FileReader fileReader = new java.io.FileReader(file);){
+        try (java.io.FileReader fileReader = new java.io.FileReader(file);) {
 
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             String line = bufferedReader.readLine();
 
-            while (line != null){
+            while (line != null) {
                 String[] pairs = line.split(": ");
                 map.put(pairs[0], pairs[1]);
                 line = bufferedReader.readLine();
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("file not found");;
         } catch (IOException e) {
             e.printStackTrace();
         }
